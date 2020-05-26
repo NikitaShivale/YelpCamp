d9019015f9edcdec108df130b921f822d71b91c3
root <nikkishivale@gmail.com>
2020-05-26 00:54:21 +0000

    initial commit

--- app.js
+++ app.js
@@ -49,7 +49,10 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-
-app.listen(3000, process.env.IP, function(){
-	console.log("Server has started");
+var PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log("Server has started");
 });
+//app.listen(3000, process.env.IP, function(){
+//	console.log("Server has started");
+//});
